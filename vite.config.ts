c37bd5bef3c9e30bfa39dcaeace8dfac4f380dd4
Phạm Ngọc Minh <nguyenhoanghiep667@gmail.com>
2025-03-19 10:24:39 +0700

    Change domain

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default ({ mode }: ConfigEnv): UserConfig => {
   const root = process.cwd()
   const env = loadEnv(mode, root)
   return {
-    base: '/',
+    base: 'DeepFake_Demo',
     server: {
       allowedHosts: ['*'],
       host: env.VITE_HOST,
